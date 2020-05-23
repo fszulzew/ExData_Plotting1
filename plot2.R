c30cbb2b9c3935f507d5ab47008b3d77cfe08b93
fszulzew <frankszul@gmx.de>
2020-05-23 11:10:10 -0700

    Plot2

---
+++ plot2.R
@@ -0,0 +1,20 @@
+#reading "household_power_consumption.txt" into HPCdatatable
+HPCdatatable <- "./household_power_consumption.txt"
+
+#reading the data from dataFile
+data <- read.table(HPCdatatable, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#selecting data points between 1/2/2007 and 2/2/2007
+HPCsubSet <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+#reading Global_active_power column
+HPCglobalActivePower <- as.numeric(HPCsubSet$Global_active_power)
+
+#Defining output png settings
+png("plot2.png", width=480, height=480)
+
+#Generating Histogram
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
